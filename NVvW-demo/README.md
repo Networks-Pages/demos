b1e1761743a2b2aa3675d48493ca558d58de9d82
Thom Castermans <t.h.a.castermans@tue.nl>
2020-10-09 14:26:37 +0200

    Cleanup; rename `redirect` to `route`; organize
    Different routes are now in separate functions. Debug mode can be controlled
    with environment variable, just like the port to use for the Node server.

--- NVvW-demo/README.md
+++ NVvW-demo/README.md
@@ -1,3 +1,9 @@
 # NVvW Demo
 
-This folder contains an interactive demo for the [NVvW study day](https://nvvw.nl/jaarvergadering/jaarvergadering-studiedag-2020/).
+This folder contains an interactive demo for the [NVvW study
+day](https://nvvw.nl/jaarvergadering/jaarvergadering-studiedag-2020/). It is
+hosted on <https://tcastermans.win.tue.nl/network/>, which has its own `app.js`
+that calls the main `route` function from the `app.js` file in this directory.
+
+To run the demo on your own machine, try `DEBUG=true PORT=8080 node app.js` and
+then open `localhost:8080` (or use another port if you wish) in your browser.
--- NVvW-demo/app.js
+++ NVvW-demo/app.js
@@ -1,61 +1,84 @@
-const http = require('http');
 const fs = require('fs');
+const http = require('http');
+const path = require('path');
 const url = require('url');
-const path = require("path");
 
-var urlPrefix = '/network';
+const DEBUG = (process.env.DEBUG === 'true');
+const URL_PREFIX = '/network';
 
 var nodes = ['a','b'];
 var neighbors = [];
-var nStartNodes = nodes.length;
 
-function redirect(req, res) {
-  if (req.url.startsWith(urlPrefix+"/getdata")) {
-    res.setHeader('Content-Type', 'application/json');
-    return res.end(JSON.stringify({
-  		"nodes": nodes,
-  		"neighbors": neighbors
-  	}));
-  } else if (req.url.startsWith(urlPrefix+"/updatedata")) {
-    var data = url.parse(req.url,true).query;
-    if (!data.n) {
-      return res.writeHead(400,{message: "Missing parameter n."}).end()
-    }
-    var newNodes = [];
-    var newNeighbors = [];
-    for (var i=data.n; i<nodes.length; i++) {
-      newNeighbors.push(neighbors[i-2])
-      newNodes.push(nodes[neighbors[i-2].id])
-    }
-    return res.end(JSON.stringify({
-      "nodes": newNodes,
-      "neighbors": newNeighbors
-    }));
-  } else if (req.url.startsWith(urlPrefix+"/addnode")) {
-	  var data = url.parse(req.url,true).query;
-	  if (data.name) {
-  		// Add a node:
-  		if (nodes.includes(data.name))
-  			return res.writeHead(400,{message: "name "+data.name+" already present",errorfield: "name"}).end();
-  		if (!nodes.includes(data.neighbor1))
-  			return res.writeHead(400,{message: "neighbor1 "+data.neighbor1+" not present",errorfield: "neighbor1"}).end();
-  		if (!nodes.includes(data.neighbor2))
-  			return res.writeHead(400,{message: "neighbor1 "+data.neighbor2+" not present",errorfield: "neighbor2"}).end();
 
-  		var id = nodes.length;
-  		nodes.push(data.name);
-  		neighbors.push({
-  			"id": id,
-  			"name": data.name,
-  			"neighbor1": nodes.indexOf(data.neighbor1),
-  			"neighbor2": nodes.indexOf(data.neighbor2),
-  		});
-  		console.log(neighbors);
-  		console.log(nodes);
-  		return res.end('okay');
-	  }
+function _addnode(req, res) {
+  var data = url.parse(req.url, true).query;
+  if (data.name) {
+    // Add a node:
+    if (nodes.includes(data.name)) {
+        return res.writeHead(400, {
+          message: `name ${data.name} already present`,
+          errorfield: "name"
+        }).end();
+    }
+    if (!nodes.includes(data.neighbor1)) {
+        return res.writeHead(400, {
+          message: `neighbor1 ${data.neighbor1} not present`,
+          errorfield: "neighbor1"
+        }).end();
+    }
+    if (!nodes.includes(data.neighbor2)) {
+        return res.writeHead(400, {
+          message: `neighbor1 ${data.neighbor2} not present`,
+          errorfield: "neighbor2"
+        }).end();
+    }
+
+    const id = nodes.length;
+    nodes.push(data.name);
+    neighbors.push({
+        "id": id,
+        "name": data.name,
+        "neighbor1": nodes.indexOf(data.neighbor1),
+        "neighbor2": nodes.indexOf(data.neighbor2),
+    });
+    return res.end('okay');
   }
-  console.log(req.url)
+}
+
+function _getdata(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  return res.end(JSON.stringify({
+    "nodes": nodes,
+    "neighbors": neighbors
+  }));
+}
+
+function _updatedata(req, res) {
+  var data = url.parse(req.url,true).query;
+  if (!data.n) {
+    return res.writeHead(400,{message: "Missing parameter n."}).end()
+  }
+  var newNodes = [];
+  var newNeighbors = [];
+  for (var i=data.n; i<nodes.length; i++) {
+    newNeighbors.push(neighbors[i-2])
+    newNodes.push(nodes[neighbors[i-2].id])
+  }
+  return res.end(JSON.stringify({
+    "nodes": newNodes,
+    "neighbors": newNeighbors
+  }));
+}
+
+
+function route(req, res) {
+  if (req.url.startsWith(`${URL_PREFIX}/getdata`)) {
+    return _getdata(req, res);
+  } else if (req.url.startsWith(`${URL_PREFIX}/updatedata`)) {
+    return _updatedata(req, res);
+  } else if (req.url.startsWith(`${URL_PREFIX}/addnode`)) {
+    return _addnode(req, res);
+  }
   fs.readFile(path.resolve(__dirname, './interface.html'), function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
@@ -64,10 +87,9 @@ function redirect(req, res) {
 }
 
 module.exports = {
-  "redirect": redirect
+  "route": route
 };
 
-var DEBUG = false;
 if (DEBUG) {
-  http.createServer(redirect).listen(8080)
+  http.createServer(route).listen(process.env.PORT);
 }
