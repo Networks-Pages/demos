c5acf477f14721e145a56d5518d25492c2ddd2fe
MartijnGosgens <martijn.gosgens@hotmail.com>
2020-10-14 17:44:33 +0200

    Implemented IDs in the url to allow 1 node per person

--- NVvW-demo/app.js
+++ NVvW-demo/app.js
@@ -7,61 +7,121 @@ const DEBUG = ((typeof process.env.DEBUG === 'undefined') || (process.env.DEBUG 
 const URL_PREFIX = '/network';
 var MAX_DEGREE = 5;
 
-var nodes = ['a','b'];
-var degrees = {};
-nodes.forEach(n => degrees[n] = 0);
+var id2node = {
+  'START_A': {
+    'id': 'START_A',
+    'name': 'A',
+    'neighbors': []
+  },
+  'START_B': {
+    'id': 'START_B',
+    'name': 'B',
+    'neighbors': []
+  }
+};
+var presentNodes = ['START_A','START_B'];
+var nStartNodes = presentNodes.length;
+
+for (var i=0; i<500;i++) {
+  id2node[`ID${i}`] = null;
+}
 var neighbors = [];
 
 
 function _addnode(req, res) {
   var data = url.parse(req.url, true).query;
-  if (data.name) {
-    // Add a node:
-    if (nodes.includes(data.name)) {
-        return res.writeHead(400, {
-          message: `name ${data.name} already present`,
-          errorfield: "name"
-        }).end();
+  if (data.name && data.id) {
+    // Check parameters
+    let newID = data.id;
+    let n1idx = parseInt(data.neighbor1);
+    let n2idx = parseInt(data.neighbor2);
+    console.log(`ID: ${newID}, neighbors: ${n1idx},${n2idx}`)
+    if (!id2node.hasOwnProperty(newID)) {
+      return res.writeHead(400, {
+        message: `The id ${newID} is not valid`,
+        errorfield: "id"
+      }).end();
     }
-    if (!nodes.includes(data.neighbor1)) {
+    if (id2node[newID] != null) {
+      return res.writeHead(400, {
+        message: `The id ${newID} has already been taken.`,
+        errorfield: "id"
+      }).end();
+    }
+    if (n1idx >= presentNodes.length) {
         return res.writeHead(400, {
-          message: `neighbor1 ${data.neighbor1} not present`,
+          message: `neighbor1 ${n1idx} does not exist`,
           errorfield: "neighbor1"
         }).end();
     }
-    if (!nodes.includes(data.neighbor2)>=MAX_DEGREE) {
+    if (n2idx >= presentNodes.length) {
         return res.writeHead(400, {
-          message: `neighbor1 ${data.neighbor2} already has ${MAX_DEGREE} connections`,
+          message: `neighbor2 ${n2idx} does not exist`,
           errorfield: "neighbor2"
         }).end();
     }
-    if (degrees[data.neighbor1]>=MAX_DEGREE) {
+    let n1ID = presentNodes[n1idx], n2ID = presentNodes[n2idx];
+    if (id2node[presentNodes[n1idx]].neighbors.length>=MAX_DEGREE) {
         return res.writeHead(400, {
-          message: `neighbor1 ${data.neighbor1} already has ${MAX_DEGREE} connections`,
+          message: `neighbor1 ${n1idx} already has ${MAX_DEGREE} connections`,
           errorfield: "neighbor1"
         }).end();
     }
+    if (id2node[presentNodes[n2idx]].neighbors.length>=MAX_DEGREE) {
+        return res.writeHead(400, {
+          message: `neighbor2 ${n2idx} already has ${MAX_DEGREE} connections`,
+          errorfield: "neighbor2"
+        }).end();
+    }
 
-    const id = nodes.length;
-    nodes.push(data.name);
+    // Add node:
+    id2node[newID] = {
+      'id': newID,
+      'name': data.name,
+      'neighbors': [n1ID,n2ID]
+    }
     neighbors.push({
-        "id": id,
-        "name": data.name,
-        "neighbor1": nodes.indexOf(data.neighbor1),
-        "neighbor2": nodes.indexOf(data.neighbor2),
+      'id': presentNodes.length,
+      'name': data.name,
+      'neighbor1': n1idx,
+      'neighbor2': n2idx
     });
-    degrees[id] = 2;
-    degrees[data.neighbor1]++;
-    degrees[data.neighbor2]++;
+    presentNodes.push(newID);
+    id2node[n1ID].neighbors.push(newID);
+    id2node[n2ID].neighbors.push(newID);
     return res.end('okay');
+  } else {
+    return res.writeHead(400, {
+      message: "Please provide a name and an ID.",
+      errorfield: "name"
+    }).end();
   }
 }
 
 function _getdata(req, res) {
   res.setHeader('Content-Type', 'application/json');
+  let nodes = [];
+  let links = [];
+  for (let i = 0; i<presentNodes.length; i++) {
+    let node = id2node[presentNodes[i]]
+    nodes.push({
+      name: node.name,
+      id: i
+    });
+    // Only add nodes with i>j because they are directed in that way
+    node.neighbors.forEach(function(nID) {
+      let j = presentNodes.indexOf(nID);
+      if (j < i) {
+        links.push({
+          source: i,
+          target: j
+        });
+      }
+    });
+  }
   return res.end(JSON.stringify({
     "nodes": nodes,
-    "neighbors": neighbors
+    "links": links
   }));
 }
 
@@ -70,14 +130,11 @@ function _updatedata(req, res) {
   if (!data.n) {
     return res.writeHead(400,{message: "Missing parameter n."}).end()
   }
-  var newNodes = [];
   var newNeighbors = [];
-  for (var i=data.n; i<nodes.length; i++) {
-    newNeighbors.push(neighbors[i-2])
-    newNodes.push(nodes[neighbors[i-2].id])
+  for (var i=data.n-nStartNodes; i<neighbors.length; i++) {
+    newNeighbors.push(neighbors[i]);
   }
   return res.end(JSON.stringify({
-    "nodes": newNodes,
     "neighbors": newNeighbors
   }));
 }
