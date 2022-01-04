6af90cefb3031b70677235ce66827d18b7ce8843
Gösgens <m.m.gosgens@tue.nl>
2022-01-04 14:22:10 +0100

    Added Netsweeper

---
+++ CustomMedia/Animations/Games/Netsweeper/eurocup2020.js
@@ -0,0 +1,332 @@
+eurocup2020 = {
+    "nodes": [
+        {
+            "name": "England",
+            "x": 0.18954160415207075,
+            "y": -0.03317790889409621
+        },
+        {
+            "name": "Sweden",
+            "x": -0.4199140486647479,
+            "y": -0.6723934486762823
+        },
+        {
+            "name": "Italy",
+            "x": -0.10928984422471166,
+            "y": 0.0156858621941022
+        },
+        {
+            "name": "Spain",
+            "x": -0.3920748263471351,
+            "y": -0.3101359134838508
+        },
+        {
+            "name": "North Macedonia",
+            "x": 0.18767227676533843,
+            "y": -0.8964425481621349
+        },
+        {
+            "name": "Switzerland",
+            "x": -0.36267465373357377,
+            "y": 0.2349041311655975
+        },
+        {
+            "name": "Turkey",
+            "x": -0.4750639417988472,
+            "y": 0.1539601803334484
+        },
+        {
+            "name": "France",
+            "x": -0.2878632040086352,
+            "y": 0.7338145815480075
+        },
+        {
+            "name": "Finland",
+            "x": 0.6519574292574036,
+            "y": 0.4936638322793967
+        },
+        {
+            "name": "Slovakia",
+            "x": -0.718936588810367,
+            "y": -0.5482726692948253
+        },
+        {
+            "name": "Poland",
+            "x": -0.6611970538371523,
+            "y": -0.6944980727985529
+        },
+        {
+            "name": "Croatia",
+            "x": 0.13350753299263574,
+            "y": -0.2786748821671829
+        },
+        {
+            "name": "Belgium",
+            "x": 0.270414952344421,
+            "y": 0.4916464336987183
+        },
+        {
+            "name": "Ukraine",
+            "x": 0.04515446775598379,
+            "y": -0.6351728286237236
+        },
+        {
+            "name": "Denmark",
+            "x": 0.37616695927662935,
+            "y": 0.25867331744284694
+        },
+        {
+            "name": "Wales",
+            "x": -0.11699502172185224,
+            "y": 0.2551406935120456
+        },
+        {
+            "name": "Scotland",
+            "x": 0.5290263571493725,
+            "y": -0.2528517026826462
+        },
+        {
+            "name": "Netherlands",
+            "x": 0.3323408241613315,
+            "y": -0.6704509261403209
+        },
+        {
+            "name": "Russia",
+            "x": 0.5665800427785607,
+            "y": 0.6285255457123212
+        },
+        {
+            "name": "Germany",
+            "x": -0.02565420779474165,
+            "y": 0.6019779213899453
+        },
+        {
+            "name": "Hungary",
+            "x": -0.1807563758466553,
+            "y": 1.0
+        },
+        {
+            "name": "Austria",
+            "x": 0.051576173743215145,
+            "y": -0.5205642627056699
+        },
+        {
+            "name": "Portugal",
+            "x": 0.018849611722897754,
+            "y": 0.8641760319281827
+        },
+        {
+            "name": "Czech Republic",
+            "x": 0.3976315346885607,
+            "y": -0.21953336757532757
+        }
+    ],
+    "links": [
+        {
+            "source": "England",
+            "target": "Croatia"
+        },
+        {
+            "source": "England",
+            "target": "Scotland"
+        },
+        {
+            "source": "England",
+            "target": "Czech Republic"
+        },
+        {
+            "source": "England",
+            "target": "Germany"
+        },
+        {
+            "source": "England",
+            "target": "Ukraine"
+        },
+        {
+            "source": "England",
+            "target": "Denmark"
+        },
+        {
+            "source": "England",
+            "target": "Italy"
+        },
+        {
+            "source": "Sweden",
+            "target": "Spain"
+        },
+        {
+            "source": "Sweden",
+            "target": "Slovakia"
+        },
+        {
+            "source": "Sweden",
+            "target": "Poland"
+        },
+        {
+            "source": "Sweden",
+            "target": "Ukraine"
+        },
+        {
+            "source": "Italy",
+            "target": "Turkey"
+        },
+        {
+            "source": "Italy",
+            "target": "Switzerland"
+        },
+        {
+            "source": "Italy",
+            "target": "Wales"
+        },
+        {
+            "source": "Italy",
+            "target": "Austria"
+        },
+        {
+            "source": "Italy",
+            "target": "Belgium"
+        },
+        {
+            "source": "Italy",
+            "target": "Spain"
+        },
+        {
+            "source": "Spain",
+            "target": "Poland"
+        },
+        {
+            "source": "Spain",
+            "target": "Slovakia"
+        },
+        {
+            "source": "Spain",
+            "target": "Croatia"
+        },
+        {
+            "source": "Spain",
+            "target": "Switzerland"
+        },
+        {
+            "source": "North Macedonia",
+            "target": "Austria"
+        },
+        {
+            "source": "North Macedonia",
+            "target": "Ukraine"
+        },
+        {
+            "source": "North Macedonia",
+            "target": "Netherlands"
+        },
+        {
+            "source": "Switzerland",
+            "target": "Wales"
+        },
+        {
+            "source": "Switzerland",
+            "target": "Turkey"
+        },
+        {
+            "source": "Switzerland",
+            "target": "France"
+        },
+        {
+            "source": "Turkey",
+            "target": "Wales"
+        },
+        {
+            "source": "France",
+            "target": "Germany"
+        },
+        {
+            "source": "France",
+            "target": "Hungary"
+        },
+        {
+            "source": "France",
+            "target": "Portugal"
+        },
+        {
+            "source": "Finland",
+            "target": "Denmark"
+        },
+        {
+            "source": "Finland",
+            "target": "Russia"
+        },
+        {
+            "source": "Finland",
+            "target": "Belgium"
+        },
+        {
+            "source": "Slovakia",
+            "target": "Poland"
+        },
+        {
+            "source": "Croatia",
+            "target": "Czech Republic"
+        },
+        {
+            "source": "Croatia",
+            "target": "Scotland"
+        },
+        {
+            "source": "Belgium",
+            "target": "Russia"
+        },
+        {
+            "source": "Belgium",
+            "target": "Denmark"
+        },
+        {
+            "source": "Belgium",
+            "target": "Portugal"
+        },
+        {
+            "source": "Ukraine",
+            "target": "Netherlands"
+        },
+        {
+            "source": "Ukraine",
+            "target": "Austria"
+        },
+        {
+            "source": "Denmark",
+            "target": "Russia"
+        },
+        {
+            "source": "Denmark",
+            "target": "Wales"
+        },
+        {
+            "source": "Denmark",
+            "target": "Czech Republic"
+        },
+        {
+            "source": "Scotland",
+            "target": "Czech Republic"
+        },
+        {
+            "source": "Netherlands",
+            "target": "Austria"
+        },
+        {
+            "source": "Netherlands",
+            "target": "Czech Republic"
+        },
+        {
+            "source": "Germany",
+            "target": "Portugal"
+        },
+        {
+            "source": "Germany",
+            "target": "Hungary"
+        },
+        {
+            "source": "Hungary",
+            "target": "Portugal"
+        }
+    ],
+    "nMines": 5,
+    "name": "eurocup2020"
+};
