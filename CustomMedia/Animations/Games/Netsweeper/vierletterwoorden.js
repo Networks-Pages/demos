21c4b1e958ac8611f1a7930e0ec9b05fafcf2bcf
MartijnGosgens <martijn.gosgens@hotmail.com>
2023-01-03 23:59:08 +0100

    Added worldcup network to Netsweeper and added FoS version

---
+++ CustomMedia/Animations/Games/Netsweeper/vierletterwoorden.js
@@ -0,0 +1,327 @@
+vierletterwoorden = {
+    "nodes": [
+        {
+            "name": "ziet",
+            "x": 0.43715440114329535,
+            "y": -0.176818171634987
+        },
+        {
+            "name": "zien",
+            "x": 0.598498706110587,
+            "y": -0.09544101062366674
+        },
+        {
+            "name": "zoet",
+            "x": 0.2408763343482641,
+            "y": -0.1730969634387307
+        },
+        {
+            "name": "mijn",
+            "x": 0.8578188828991066,
+            "y": -0.22703498395805338
+        },
+        {
+            "name": "niet",
+            "x": 0.48635720469320276,
+            "y": -0.2758899653326134
+        },
+        {
+            "name": "meer",
+            "x": -0.9751433318836409,
+            "y": -0.16662395221678575
+        },
+        {
+            "name": "heel",
+            "x": -0.34929228410818863,
+            "y": 0.09151143709739328
+        },
+        {
+            "name": "uien",
+            "x": 0.5513670313994894,
+            "y": 0.07093088382329431
+        },
+        {
+            "name": "keer",
+            "x": -1.0,
+            "y": -0.11489300640687143
+        },
+        {
+            "name": "veel",
+            "x": -0.16472894592521706,
+            "y": 0.1374371242605298
+        },
+        {
+            "name": "heet",
+            "x": -0.5668568874393907,
+            "y": 0.0443806341104185
+        },
+        {
+            "name": "zijn",
+            "x": 0.7649486146060749,
+            "y": -0.1608901063054201
+        },
+        {
+            "name": "voet",
+            "x": 0.1353953178981992,
+            "y": -0.12105432052100887
+        },
+        {
+            "name": "weer",
+            "x": -0.8938438522697326,
+            "y": -0.09483131894341132
+        },
+        {
+            "name": "uren",
+            "x": 0.6518513371006611,
+            "y": 0.12628812763954764
+        },
+        {
+            "name": "viel",
+            "x": 0.038304496192184956,
+            "y": 0.2022670480465615
+        },
+        {
+            "name": "doet",
+            "x": 0.063609292518699,
+            "y": -0.15990091869468698
+        },
+        {
+            "name": "deel",
+            "x": -0.2225302911069129,
+            "y": 0.0523350617516109
+        },
+        {
+            "name": "geen",
+            "x": -0.3238392197564024,
+            "y": 0.28943502991817954
+        },
+        {
+            "name": "voeg",
+            "x": 0.04714741028940353,
+            "y": -0.021519466833814217
+        },
+        {
+            "name": "doen",
+            "x": -0.03873748537419737,
+            "y": -0.18296674589129505
+        },
+        {
+            "name": "uier",
+            "x": 0.40571434525684136,
+            "y": 0.17906290839819436
+        },
+        {
+            "name": "geef",
+            "x": -0.37097691100368485,
+            "y": 0.26070221935011
+        },
+        {
+            "name": "lijn",
+            "x": 0.8785846818655939,
+            "y": -0.17408144931451258
+        },
+        {
+            "name": "weet",
+            "x": -0.7419538576994152,
+            "y": -0.026854271680798635
+        },
+        {
+            "name": "hebt",
+            "x": -0.6611840030265692,
+            "y": 0.0852920643468149
+        },
+        {
+            "name": "geel",
+            "x": -0.2879572197340173,
+            "y": 0.1749708793978918
+        },
+        {
+            "name": "doel",
+            "x": -0.09103402236350056,
+            "y": -0.08987491315253043
+        },
+        {
+            "name": "veeg",
+            "x": -0.055542991505368546,
+            "y": 0.06833204175327438
+        },
+        {
+            "name": "hier",
+            "x": 0.34522623705124517,
+            "y": 0.25640500602487987
+        },
+        {
+            "name": "vier",
+            "x": 0.24076700982339064,
+            "y": 0.2224210990304852
+        }
+    ],
+    "links": [
+        {
+            "source": "ziet",
+            "target": "zien"
+        },
+        {
+            "source": "ziet",
+            "target": "niet"
+        },
+        {
+            "source": "ziet",
+            "target": "zoet"
+        },
+        {
+            "source": "zien",
+            "target": "zijn"
+        },
+        {
+            "source": "zien",
+            "target": "uien"
+        },
+        {
+            "source": "zoet",
+            "target": "voet"
+        },
+        {
+            "source": "zoet",
+            "target": "doet"
+        },
+        {
+            "source": "mijn",
+            "target": "zijn"
+        },
+        {
+            "source": "mijn",
+            "target": "lijn"
+        },
+        {
+            "source": "meer",
+            "target": "keer"
+        },
+        {
+            "source": "meer",
+            "target": "weer"
+        },
+        {
+            "source": "heel",
+            "target": "deel"
+        },
+        {
+            "source": "heel",
+            "target": "geel"
+        },
+        {
+            "source": "heel",
+            "target": "veel"
+        },
+        {
+            "source": "heel",
+            "target": "heet"
+        },
+        {
+            "source": "uien",
+            "target": "uier"
+        },
+        {
+            "source": "uien",
+            "target": "uren"
+        },
+        {
+            "source": "keer",
+            "target": "weer"
+        },
+        {
+            "source": "veel",
+            "target": "deel"
+        },
+        {
+            "source": "veel",
+            "target": "geel"
+        },
+        {
+            "source": "veel",
+            "target": "veeg"
+        },
+        {
+            "source": "veel",
+            "target": "viel"
+        },
+        {
+            "source": "heet",
+            "target": "weet"
+        },
+        {
+            "source": "heet",
+            "target": "hebt"
+        },
+        {
+            "source": "zijn",
+            "target": "lijn"
+        },
+        {
+            "source": "voet",
+            "target": "doet"
+        },
+        {
+            "source": "voet",
+            "target": "voeg"
+        },
+        {
+            "source": "weer",
+            "target": "weet"
+        },
+        {
+            "source": "viel",
+            "target": "vier"
+        },
+        {
+            "source": "doet",
+            "target": "doel"
+        },
+        {
+            "source": "doet",
+            "target": "doen"
+        },
+        {
+            "source": "deel",
+            "target": "geel"
+        },
+        {
+            "source": "deel",
+            "target": "doel"
+        },
+        {
+            "source": "geen",
+            "target": "geel"
+        },
+        {
+            "source": "geen",
+            "target": "geef"
+        },
+        {
+            "source": "voeg",
+            "target": "veeg"
+        },
+        {
+            "source": "doen",
+            "target": "doel"
+        },
+        {
+            "source": "uier",
+            "target": "hier"
+        },
+        {
+            "source": "uier",
+            "target": "vier"
+        },
+        {
+            "source": "geef",
+            "target": "geel"
+        },
+        {
+            "source": "hier",
+            "target": "vier"
+        }
+    ],
+    "nMines": 4,
+    "name": "vierletterwoorden"
+};
---
+++ CustomMedia/Animations/Games/Netsweeper/worldcup2022.js
@@ -0,0 +1,420 @@
+worldcup2022 = {
+    "nodes": [
+        {
+            "name": "Germany",
+            "x": 0.5087233413885567,
+            "y": 0.7424845966856327
+        },
+        {
+            "name": "Belgium",
+            "x": -0.05167953258413486,
+            "y": 0.426091160810552
+        },
+        {
+            "name": "Senegal",
+            "x": -0.162257771201146,
+            "y": -0.6785159656289909
+        },
+        {
+            "name": "Portugal",
+            "x": -0.4115195717597761,
+            "y": 0.547353814863723
+        },
+        {
+            "name": "Morocco",
+            "x": 0.08240867844967267,
+            "y": 0.31446942340067147
+        },
+        {
+            "name": "USA",
+            "x": 0.03197110261296549,
+            "y": -0.8562037891455487
+        },
+        {
+            "name": "Cameroon",
+            "x": -0.8344369189812054,
+            "y": 0.3291721279103822
+        },
+        {
+            "name": "Qatar",
+            "x": -0.32033878581560193,
+            "y": -0.5539181417254524
+        },
+        {
+            "name": "France",
+            "x": 0.3494732753332576,
+            "y": -0.18193568775628824
+        },
+        {
+            "name": "Wales",
+            "x": 0.16617188291339904,
+            "y": -0.9890205783059752
+        },
+        {
+            "name": "Denmark",
+            "x": 0.681368497765545,
+            "y": -0.20558859142468883
+        },
+        {
+            "name": "Iran",
+            "x": 0.04119631789947345,
+            "y": -1.0
+        },
+        {
+            "name": "Netherlands",
+            "x": -0.04576830669236805,
+            "y": -0.5562278311190819
+        },
+        {
+            "name": "Uruguay",
+            "x": -0.6839042708731057,
+            "y": 0.7261010255857211
+        },
+        {
+            "name": "Korea Republic",
+            "x": -0.6069984273534675,
+            "y": 0.597682773691335
+        },
+        {
+            "name": "Canada",
+            "x": 0.0632997884684622,
+            "y": 0.4569419769438459
+        },
+        {
+            "name": "Ecuador",
+            "x": -0.24082997733340047,
+            "y": -0.4880200476692349
+        },
+        {
+            "name": "Poland",
+            "x": 0.4344034261976284,
+            "y": -0.3793225809681645
+        },
+        {
+            "name": "Saudi Arabia",
+            "x": 0.5290332784202372,
+            "y": -0.4699294563210042
+        },
+        {
+            "name": "Ghana",
+            "x": -0.57757840900457,
+            "y": 0.7824407963893851
+        },
+        {
+            "name": "Tunisia",
+            "x": 0.645243798131153,
+            "y": -0.08848073667479343
+        },
+        {
+            "name": "Japan",
+            "x": 0.27106637282379364,
+            "y": 0.5869515204376138
+        },
+        {
+            "name": "Serbia",
+            "x": -0.7786356155879717,
+            "y": 0.23344658029433932
+        },
+        {
+            "name": "Brazil",
+            "x": -0.5148035799124212,
+            "y": 0.3200944255420412
+        },
+        {
+            "name": "Costa Rica",
+            "x": 0.4132108028955705,
+            "y": 0.8157447300253121
+        },
+        {
+            "name": "Switzerland",
+            "x": -0.6844864568989225,
+            "y": 0.39832255894640034
+        },
+        {
+            "name": "Australia",
+            "x": 0.5244742387872269,
+            "y": -0.1859552939865757
+        },
+        {
+            "name": "England",
+            "x": 0.13954660844251493,
+            "y": -0.6899290217914615
+        },
+        {
+            "name": "Mexico",
+            "x": 0.4212747166453755,
+            "y": -0.522618724889546
+        },
+        {
+            "name": "Croatia",
+            "x": -0.005135727074079345,
+            "y": 0.23096285547416656
+        },
+        {
+            "name": "Argentina",
+            "x": 0.24847143374370076,
+            "y": -0.2675310690280425
+        },
+        {
+            "name": "Spain",
+            "x": 0.36703579015363735,
+            "y": 0.6049371494337291
+        }
+    ],
+    "links": [
+        {
+            "source": "Germany",
+            "target": "Japan"
+        },
+        {
+            "source": "Germany",
+            "target": "Spain"
+        },
+        {
+            "source": "Germany",
+            "target": "Costa Rica"
+        },
+        {
+            "source": "Belgium",
+            "target": "Canada"
+        },
+        {
+            "source": "Belgium",
+            "target": "Morocco"
+        },
+        {
+            "source": "Belgium",
+            "target": "Croatia"
+        },
+        {
+            "source": "Senegal",
+            "target": "Netherlands"
+        },
+        {
+            "source": "Senegal",
+            "target": "Qatar"
+        },
+        {
+            "source": "Senegal",
+            "target": "Ecuador"
+        },
+        {
+            "source": "Senegal",
+            "target": "England"
+        },
+        {
+            "source": "Portugal",
+            "target": "Ghana"
+        },
+        {
+            "source": "Portugal",
+            "target": "Uruguay"
+        },
+        {
+            "source": "Portugal",
+            "target": "Korea Republic"
+        },
+        {
+            "source": "Portugal",
+            "target": "Switzerland"
+        },
+        {
+            "source": "Portugal",
+            "target": "Morocco"
+        },
+        {
+            "source": "Morocco",
+            "target": "Croatia"
+        },
+        {
+            "source": "Morocco",
+            "target": "Canada"
+        },
+        {
+            "source": "Morocco",
+            "target": "Spain"
+        },
+        {
+            "source": "Morocco",
+            "target": "France"
+        },
+        {
+            "source": "USA",
+            "target": "Wales"
+        },
+        {
+            "source": "USA",
+            "target": "England"
+        },
+        {
+            "source": "USA",
+            "target": "Iran"
+        },
+        {
+            "source": "USA",
+            "target": "Netherlands"
+        },
+        {
+            "source": "Cameroon",
+            "target": "Switzerland"
+        },
+        {
+            "source": "Cameroon",
+            "target": "Serbia"
+        },
+        {
+            "source": "Cameroon",
+            "target": "Brazil"
+        },
+        {
+            "source": "Qatar",
+            "target": "Ecuador"
+        },
+        {
+            "source": "Qatar",
+            "target": "Netherlands"
+        },
+        {
+            "source": "France",
+            "target": "Australia"
+        },
+        {
+            "source": "France",
+            "target": "Denmark"
+        },
+        {
+            "source": "France",
+            "target": "Tunisia"
+        },
+        {
+            "source": "France",
+            "target": "Poland"
+        },
+        {
+            "source": "France",
+            "target": "England"
+        },
+        {
+            "source": "France",
+            "target": "Argentina"
+        },
+        {
+            "source": "Wales",
+            "target": "Iran"
+        },
+        {
+            "source": "Wales",
+            "target": "England"
+        },
+        {
+            "source": "Denmark",
+            "target": "Tunisia"
+        },
+        {
+            "source": "Denmark",
+            "target": "Australia"
+        },
+        {
+            "source": "Iran",
+            "target": "England"
+        },
+        {
+            "source": "Netherlands",
+            "target": "Ecuador"
+        },
+        {
+            "source": "Netherlands",
+            "target": "Argentina"
+        },
+        {
+            "source": "Uruguay",
+            "target": "Korea Republic"
+        },
+        {
+            "source": "Uruguay",
+            "target": "Ghana"
+        },
+        {
+            "source": "Korea Republic",
+            "target": "Ghana"
+        },
+        {
+            "source": "Korea Republic",
+            "target": "Brazil"
+        },
+        {
+            "source": "Canada",
+            "target": "Croatia"
+        },
+        {
+            "source": "Poland",
+            "target": "Mexico"
+        },
+        {
+            "source": "Poland",
+            "target": "Saudi Arabia"
+        },
+        {
+            "source": "Poland",
+            "target": "Argentina"
+        },
+        {
+            "source": "Saudi Arabia",
+            "target": "Argentina"
+        },
+        {
+            "source": "Saudi Arabia",
+            "target": "Mexico"
+        },
+        {
+            "source": "Tunisia",
+            "target": "Australia"
+        },
+        {
+            "source": "Japan",
+            "target": "Costa Rica"
+        },
+        {
+            "source": "Japan",
+            "target": "Spain"
+        },
+        {
+            "source": "Japan",
+            "target": "Croatia"
+        },
+        {
+            "source": "Serbia",
+            "target": "Brazil"
+        },
+        {
+            "source": "Serbia",
+            "target": "Switzerland"
+        },
+        {
+            "source": "Brazil",
+            "target": "Switzerland"
+        },
+        {
+            "source": "Brazil",
+            "target": "Croatia"
+        },
+        {
+            "source": "Costa Rica",
+            "target": "Spain"
+        },
+        {
+            "source": "Australia",
+            "target": "Argentina"
+        },
+        {
+            "source": "Mexico",
+            "target": "Argentina"
+        },
+        {
+            "source": "Croatia",
+            "target": "Argentina"
+        }
+    ],
+    "nMines": 3,
+    "name": "worldcup2022"
+};
