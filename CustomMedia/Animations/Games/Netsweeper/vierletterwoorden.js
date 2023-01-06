0bf09451d1dbef3bdbcc4f34e0fb335cd6e2a87e
MartijnGosgens <martijn.gosgens@hotmail.com>
2023-01-06 13:53:18 +0100

    Updated vierletterwoordennetwerk

--- CustomMedia/Animations/Games/Netsweeper/vierletterwoorden.js
+++ CustomMedia/Animations/Games/Netsweeper/vierletterwoorden.js
@@ -1,197 +1,180 @@
 vierletterwoorden = {
     "nodes": [
         {
-            "name": "ziet",
-            "x": 0.43715440114329535,
-            "y": -0.176818171634987
-        },
-        {
-            "name": "zien",
-            "x": 0.598498706110587,
-            "y": -0.09544101062366674
-        },
-        {
-            "name": "zoet",
-            "x": 0.2408763343482641,
-            "y": -0.1730969634387307
-        },
-        {
-            "name": "mijn",
-            "x": 0.8578188828991066,
-            "y": -0.22703498395805338
-        },
-        {
-            "name": "niet",
-            "x": 0.48635720469320276,
-            "y": -0.2758899653326134
-        },
-        {
-            "name": "meer",
-            "x": -0.9751433318836409,
-            "y": -0.16662395221678575
-        },
-        {
             "name": "heel",
-            "x": -0.34929228410818863,
-            "y": 0.09151143709739328
-        },
-        {
-            "name": "uien",
-            "x": 0.5513670313994894,
-            "y": 0.07093088382329431
-        },
-        {
-            "name": "keer",
-            "x": -1.0,
-            "y": -0.11489300640687143
-        },
-        {
-            "name": "veel",
-            "x": -0.16472894592521706,
-            "y": 0.1374371242605298
+            "x": 0.31333270242063976,
+            "y": 0.17692072719720123
         },
         {
             "name": "heet",
-            "x": -0.5668568874393907,
-            "y": 0.0443806341104185
+            "x": 0.5331967845750643,
+            "y": 0.21982418316193794
         },
         {
-            "name": "zijn",
-            "x": 0.7649486146060749,
-            "y": -0.1608901063054201
-        },
-        {
-            "name": "voet",
-            "x": 0.1353953178981992,
-            "y": -0.12105432052100887
-        },
-        {
-            "name": "weer",
-            "x": -0.8938438522697326,
-            "y": -0.09483131894341132
-        },
-        {
-            "name": "uren",
-            "x": 0.6518513371006611,
-            "y": 0.12628812763954764
-        },
-        {
-            "name": "viel",
-            "x": 0.038304496192184956,
-            "y": 0.2022670480465615
+            "name": "meer",
+            "x": 1.0,
+            "y": 0.23639293184970162
         },
         {
             "name": "doet",
-            "x": 0.063609292518699,
-            "y": -0.15990091869468698
+            "x": 0.024686925311715624,
+            "y": -0.23711307462581777
         },
         {
-            "name": "deel",
-            "x": -0.2225302911069129,
-            "y": 0.0523350617516109
+            "name": "uien",
+            "x": -0.5434825625966164,
+            "y": -0.11376238978465585
         },
         {
             "name": "geen",
-            "x": -0.3238392197564024,
-            "y": 0.28943502991817954
+            "x": 0.3279349127767165,
+            "y": 0.28551930322398766
         },
         {
-            "name": "voeg",
-            "x": 0.04714741028940353,
-            "y": -0.021519466833814217
+            "name": "veel",
+            "x": 0.12420708293683562,
+            "y": 0.15285601067526564
         },
         {
-            "name": "doen",
-            "x": -0.03873748537419737,
-            "y": -0.18296674589129505
+            "name": "uren",
+            "x": -0.6551248802001977,
+            "y": -0.08720794758615204
         },
         {
-            "name": "uier",
-            "x": 0.40571434525684136,
-            "y": 0.17906290839819436
-        },
-        {
-            "name": "geef",
-            "x": -0.37097691100368485,
-            "y": 0.26070221935011
-        },
-        {
-            "name": "lijn",
-            "x": 0.8785846818655939,
-            "y": -0.17408144931451258
-        },
-        {
-            "name": "weet",
-            "x": -0.7419538576994152,
-            "y": -0.026854271680798635
-        },
-        {
-            "name": "hebt",
-            "x": -0.6611840030265692,
-            "y": 0.0852920643468149
+            "name": "niet",
+            "x": -0.38625372053046314,
+            "y": -0.4322079863309703
         },
         {
             "name": "geel",
-            "x": -0.2879572197340173,
-            "y": 0.1749708793978918
+            "x": 0.22675668154044173,
+            "y": 0.23006300035478627
         },
         {
-            "name": "doel",
-            "x": -0.09103402236350056,
-            "y": -0.08987491315253043
+            "name": "viel",
+            "x": -0.08688872169635206,
+            "y": 0.15374636685077428
         },
         {
             "name": "veeg",
-            "x": -0.055542991505368546,
-            "y": 0.06833204175327438
+            "x": 0.05005230798326025,
+            "y": 0.03926692360023872
+        },
+        {
+            "name": "geef",
+            "x": 0.22295850144603738,
+            "y": 0.34841437634647326
+        },
+        {
+            "name": "mijn",
+            "x": -0.8011445728035838,
+            "y": -0.43226771950268394
+        },
+        {
+            "name": "deel",
+            "x": 0.20318403660423162,
+            "y": 0.09889545059245124
+        },
+        {
+            "name": "voeg",
+            "x": -0.019478306593370544,
+            "y": -0.08081194023183758
+        },
+        {
+            "name": "hebt",
+            "x": 0.6074075450480014,
+            "y": 0.28385877211802457
         },
         {
             "name": "hier",
-            "x": 0.34522623705124517,
-            "y": 0.25640500602487987
+            "x": -0.394528663405938,
+            "y": 0.12002767310399945
+        },
+        {
+            "name": "zien",
+            "x": -0.5468421900203556,
+            "y": -0.2861475860258761
+        },
+        {
+            "name": "ziet",
+            "x": -0.36902207452773167,
+            "y": -0.32130545265771915
+        },
+        {
+            "name": "doel",
+            "x": 0.11046725454269561,
+            "y": -0.06214309779868193
+        },
+        {
+            "name": "weer",
+            "x": 0.8918478601926962,
+            "y": 0.2545451392724912
+        },
+        {
+            "name": "voet",
+            "x": -0.0906001089230408,
+            "y": -0.19329381523436753
+        },
+        {
+            "name": "weet",
+            "x": 0.724207144802161,
+            "y": 0.23748809252159864
         },
         {
             "name": "vier",
-            "x": 0.24076700982339064,
-            "y": 0.2224210990304852
+            "x": -0.2853503230752529,
+            "y": 0.11592747544679843
+        },
+        {
+            "name": "lijn",
+            "x": -0.7175149971995486,
+            "y": -0.47898981027245857
+        },
+        {
+            "name": "zijn",
+            "x": -0.6932691083516459,
+            "y": -0.3910726782711724
+        },
+        {
+            "name": "uier",
+            "x": -0.4318761669845687,
+            "y": 0.029118923848693315
+        },
+        {
+            "name": "keer",
+            "x": 0.9943901838658731,
+            "y": 0.2946402646617475
+        },
+        {
+            "name": "zoet",
+            "x": -0.18325352713770465,
+            "y": -0.26118211650377776
         }
     ],
     "links": [
         {
-            "source": "ziet",
-            "target": "zien"
+            "source": "heel",
+            "target": "heet"
         },
         {
-            "source": "ziet",
-            "target": "niet"
+            "source": "heel",
+            "target": "veel"
         },
         {
-            "source": "ziet",
-            "target": "zoet"
+            "source": "heel",
+            "target": "geel"
         },
         {
-            "source": "zien",
-            "target": "zijn"
+            "source": "heel",
+            "target": "deel"
         },
         {
-            "source": "zien",
-            "target": "uien"
+            "source": "heet",
+            "target": "hebt"
         },
         {
-            "source": "zoet",
-            "target": "voet"
-        },
-        {
-            "source": "zoet",
-            "target": "doet"
-        },
-        {
-            "source": "mijn",
-            "target": "zijn"
-        },
-        {
-            "source": "mijn",
-            "target": "lijn"
+            "source": "heet",
+            "target": "weet"
         },
         {
             "source": "meer",
@@ -202,92 +185,28 @@ vierletterwoorden = {
             "target": "weer"
         },
         {
-            "source": "heel",
-            "target": "deel"
+            "source": "doet",
+            "target": "voet"
         },
         {
-            "source": "heel",
-            "target": "geel"
+            "source": "doet",
+            "target": "zoet"
         },
         {
-            "source": "heel",
-            "target": "veel"
-        },
-        {
-            "source": "heel",
-            "target": "heet"
-        },
-        {
-            "source": "uien",
-            "target": "uier"
+            "source": "doet",
+            "target": "doel"
         },
         {
             "source": "uien",
             "target": "uren"
         },
         {
-            "source": "keer",
-            "target": "weer"
+            "source": "uien",
+            "target": "zien"
         },
         {
-            "source": "veel",
-            "target": "deel"
-        },
-        {
-            "source": "veel",
-            "target": "geel"
-        },
-        {
-            "source": "veel",
-            "target": "veeg"
-        },
-        {
-            "source": "veel",
-            "target": "viel"
-        },
-        {
-            "source": "heet",
-            "target": "weet"
-        },
-        {
-            "source": "heet",
-            "target": "hebt"
-        },
-        {
-            "source": "zijn",
-            "target": "lijn"
-        },
-        {
-            "source": "voet",
-            "target": "doet"
-        },
-        {
-            "source": "voet",
-            "target": "voeg"
-        },
-        {
-            "source": "weer",
-            "target": "weet"
-        },
-        {
-            "source": "viel",
-            "target": "vier"
-        },
-        {
-            "source": "doet",
-            "target": "doel"
-        },
-        {
-            "source": "doet",
-            "target": "doen"
-        },
-        {
-            "source": "deel",
-            "target": "geel"
-        },
-        {
-            "source": "deel",
-            "target": "doel"
+            "source": "uien",
+            "target": "uier"
         },
         {
             "source": "geen",
@@ -298,28 +217,96 @@ vierletterwoorden = {
             "target": "geef"
         },
         {
-            "source": "voeg",
+            "source": "veel",
+            "target": "viel"
+        },
+        {
+            "source": "veel",
             "target": "veeg"
         },
         {
-            "source": "doen",
-            "target": "doel"
+            "source": "veel",
+            "target": "geel"
         },
         {
-            "source": "uier",
-            "target": "hier"
+            "source": "veel",
+            "target": "deel"
         },
         {
-            "source": "uier",
+            "source": "niet",
+            "target": "ziet"
+        },
+        {
+            "source": "geel",
+            "target": "geef"
+        },
+        {
+            "source": "geel",
+            "target": "deel"
+        },
+        {
+            "source": "viel",
             "target": "vier"
         },
         {
-            "source": "geef",
-            "target": "geel"
+            "source": "veeg",
+            "target": "voeg"
+        },
+        {
+            "source": "mijn",
+            "target": "zijn"
+        },
+        {
+            "source": "mijn",
+            "target": "lijn"
+        },
+        {
+            "source": "deel",
+            "target": "doel"
+        },
+        {
+            "source": "voeg",
+            "target": "voet"
         },
         {
             "source": "hier",
             "target": "vier"
+        },
+        {
+            "source": "hier",
+            "target": "uier"
+        },
+        {
+            "source": "zien",
+            "target": "zijn"
+        },
+        {
+            "source": "zien",
+            "target": "ziet"
+        },
+        {
+            "source": "ziet",
+            "target": "zoet"
+        },
+        {
+            "source": "weer",
+            "target": "keer"
+        },
+        {
+            "source": "weer",
+            "target": "weet"
+        },
+        {
+            "source": "voet",
+            "target": "zoet"
+        },
+        {
+            "source": "vier",
+            "target": "uier"
+        },
+        {
+            "source": "lijn",
+            "target": "zijn"
         }
     ],
     "nMines": 4,
